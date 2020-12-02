84f98518602568c37af6a90daa4bcebeb8c28f6b
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2020-12-02 23:25:34 +0600

    Update MainClassLLManual_singlycircularll.java

--- MainClassLLManual_singlycircularll.java
+++ MainClassLLManual_singlycircularll.java
@@ -95,7 +95,7 @@ class LinkedList {
 }
 
 
-public class MainClassLL_singlycircularll {
+public class MainClassLLManual_singlycircularll {
 	public static void main(String[] args) {
 		
 		LinkedList l = new LinkedList();
